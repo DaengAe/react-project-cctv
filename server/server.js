6ea6f18cf8fa34faeacfaa604d9c13baaf373da3
daengae <dladmsxor434@naver.com>
2024-11-11 20:14:55 +0900

    commit

--- server/server.js
+++ server/server.js
@@ -3,11 +3,20 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors());
+app.use(express.static('build'));
 
 const fs = require('fs');
-const dataBuffer = fs.readFileSync('../JsonData/cctvData.json');
+const dataBuffer = fs.readFileSync('./build/JsonData/cctvData.json');
 const dataJSON = dataBuffer.toString();
 const dataList = JSON.parse(dataJSON);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/build/index.html');
+});
+
+app.listen(3000, () => {
+    console.log("client start!");
+});
 
 app.get('/api/cctv', (req, res) => {
     res.json(dataList);
